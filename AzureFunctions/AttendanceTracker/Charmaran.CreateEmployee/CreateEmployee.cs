31ede0cfb6462f737f837ea7cafd10f0466af997
Harold Smith <smithhe95@protonmail.com>
2024-09-12 22:57:28 -0400

    Added base functionality to test the function

--- AzureFunctions/AttendanceTracker/Charmaran.CreateEmployee/CreateEmployee.cs
+++ AzureFunctions/AttendanceTracker/Charmaran.CreateEmployee/CreateEmployee.cs
@@ -1,22 +1,31 @@
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Charmaran.Application.Contracts.AttendanceTracker;
+using Charmaran.Shared.AttendanceTracker.Requests;
+using Charmaran.Shared.AttendanceTracker.Responses.Employee;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker.Http;
 
 namespace Charmaran.CreateEmployee
 {
     public class CreateEmployee
     {
         private readonly ILogger<CreateEmployee> _logger;
+        private readonly IEmployeeService _employeeService;
 
         /// <summary>
         /// Creates a new instance of the <see cref="CreateEmployee"/> class.
         /// </summary>
         /// <param name="logger">The logger to use.</param>
-        public CreateEmployee(ILogger<CreateEmployee> logger)
+        /// <param name="employeeService">The employee service to use.</param>
+        public CreateEmployee(ILogger<CreateEmployee> logger, IEmployeeService employeeService)
         {
             // Set the logger for the class
             _logger = logger;
+            this._employeeService = employeeService;
         }
 
         /// <summary>
@@ -25,11 +34,29 @@ namespace Charmaran.CreateEmployee
         /// <param name="req">The HTTP request.</param>
         /// <returns>The HTTP response.</returns>
         [Function("CreateEmployee")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            // TODO: Implement the logic to create a new employee
-            return new OkObjectResult("Welcome to Azure Functions!");
+            // Log the request
+            _logger.LogInformation("Handling request for creating a new employee...");
+            
+            // Parse the request body
+            CreateEmployeeApiRequest? body;
+            try
+            {
+                body = JsonSerializer.Deserialize<CreateEmployeeApiRequest>(req.Body);  
+            }
+            catch (Exception e)
+            {
+                // Log the error and return an error response
+                this._logger.LogError(e, "Error parsing request body");
+                return new BadRequestObjectResult("Invalid request body");
+            }
+            
+            // Call the service
+            CreateEmployeeResponse response = await _employeeService.CreateEmployeeAsync(body?.Name ?? string.Empty);
+            
+            // Return the result
+            return new OkObjectResult(response);
         }
 
     }
--- AzureFunctions/AttendanceTracker/Charmaran.CreateEmployee/Program.cs
+++ AzureFunctions/AttendanceTracker/Charmaran.CreateEmployee/Program.cs
@@ -1,13 +1,23 @@
+using Charmaran.Application;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 IHost host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        config.AddJsonFile("local.settings.json", optional: true);
+        config.AddEnvironmentVariables();
+    })
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        
+        //services.AddAttendanceTrackerServices();
     })
     .Build();
 
