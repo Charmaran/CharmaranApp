27c116cb5ee7d5de904b2ac5a16a31dc260ec4b4
Harold Smith <smithhe95@protonmail.com>
2024-11-23 21:35:08 -0500

    Added UpdateEmployee Endpoint

---
+++ Api/Charmaran.FastEndpoints/Endpoints/AttendanceTracker/Employee/UpdateEmployeeEndpoint.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Charmaran.Application.Contracts.AttendanceTracker;
+using Charmaran.Shared.AttendanceTracker.Requests.Employee;
+using Charmaran.Shared.AttendanceTracker.Responses.Employee;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Charmaran.FastEndpoints.Endpoints.AttendanceTracker.Employee
+{
+    public class UpdateEmployeeEndpoint : Endpoint<UpdateEmployeeApiRequest, UpdateEmployeeResponse>
+    {
+        private readonly ILogger<UpdateEmployeeEndpoint> _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public UpdateEmployeeEndpoint(ILogger<UpdateEmployeeEndpoint> logger, IEmployeeService employeeService)
+        {
+            this._logger = logger;
+            this._employeeService = employeeService;
+        }
+
+        public override void Configure()
+        {
+            this.AllowAnonymous();
+            this.Put("api/employee/update");
+            //this.Policies(PolicyNames._adminPolicy);
+            this.Options(o => o.WithTags("Employee"));
+        }
+        
+        public override async Task HandleAsync(UpdateEmployeeApiRequest req, CancellationToken ct)
+        {
+            this._logger.LogInformation("Handling Update Employee Request");
+
+            UpdateEmployeeResponse updateEmployeeResponse;
+            try
+            {
+                updateEmployeeResponse = await this._employeeService.UpdateEmployeeAsync(req.Id, req.Name);
+            }
+            catch (Exception e)
+            {
+                this._logger.LogError(e, "Error updating employee");
+                updateEmployeeResponse = new UpdateEmployeeResponse
+                {
+                    Success = false,
+                    Message = "Unexpected Error Occurred"
+                };
+            }
+            
+            await this.SendAsync(updateEmployeeResponse, cancellation: ct);
+        }
+    }
+}
